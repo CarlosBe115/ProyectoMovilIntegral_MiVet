0fec53334ff7b5659937570eb4284cb41f9c6afe
Carlos Andres Be Garrido <carlosandresbe115@gmail.com>
2022-11-29 12:27:39 -0600

    add sinvacuna

--- proyecto_appmovil/lib/vacunas/sinvacuna.dart
+++ proyecto_appmovil/lib/vacunas/sinvacuna.dart
@@ -1,30 +1,22 @@
-/*import 'dart:convert';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:proyecto_appmovil/models/animales.dart';
+import 'package:proyecto_appmovil/models/animalesinvacuna.dart';
 import 'package:proyecto_appmovil/models/vacuna.dart';
 import 'package:proyecto_appmovil/models/vacunaspost.dart';
 
 // ignore: camel_case_types
 class sinvacuna extends StatefulWidget {
-  sinvacuna({
-    super.key,
-    required this.id,
-    required this.apodo,
-    required this.especie,
-  });
-  final String? id;
-  final String? apodo;
-  final int? especie;
+  sinvacuna({Key? key}) : super(key: key);
 
   State<sinvacuna> createState() => _listState();
 }
 
 class _listState extends State<sinvacuna> {
-  late Future<List<Animales>> animales;
-  late Future<List<Vacuna>> vacunas;
+  late Future<List<Animalesinvacuna>> animales;
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +24,8 @@ class _listState extends State<sinvacuna> {
       appBar: AppBar(
         title: const Text('Lista de Vacunas'),
       ),
-      body: FutureBuilder<List<Vacuna>>(
-        future: vacunas,
+      body: FutureBuilder<List<Animalesinvacuna>>(
+        future: animales,
         builder: (context, snap) {
           if (snap.hasData) {
             return ListView.builder(
@@ -43,9 +35,8 @@ class _listState extends State<sinvacuna> {
                   return Column(
                     children: [
                       ListTile(
-                          title: Text(snap.data![i].vacuna),
-                          subtitle: Text(
-                              DateFormat.yMMMEd().format(snap.data![i].fecha))),
+                          title: Text(snap.data![i].apodo),
+                          subtitle: const Text("Necesita Vacuna")),
                       const Divider(),
                       const Text(
                         "",
@@ -64,40 +55,18 @@ class _listState extends State<sinvacuna> {
   @override
   void initState() {
     super.initState();
-    animales = _getAnimalFiltro1();
-    vacunas = _getAnimalFiltro2();
+    animales = _getAnimalSinVacuna();
   }
 
-  Future<List<Animales>> _getAnimalFiltro1() async {
-    final response1 =
-        await http.get(Uri.parse('https://mivetapi.somee.com/api/animal/pro'));
-    final jsonData1 = List.from(jsonDecode(response1.body));
-
-    final response2 =
-        await http.get(Uri.parse('https://mivetapi.somee.com/api/Vacuna'));
-    final jsonData2 = List.from(jsonDecode(response2.body));
-
-    List Filtro = [];
-    for (int i = 0; i < jsonData1.length;i ++){
-      for(int j = 0; j < jsonData2.length; j ++){
-        if (jsonData1.){}
-      }
-    } 
-    List<Animales> animales = [];
-    jsonData1.forEach((element) {
-      final Animales animals = Animales.fromJson(element);
+  Future<List<Animalesinvacuna>> _getAnimalSinVacuna() async {
+    final response = await http.get(
+        Uri.parse('https://mivetapi.somee.com/api/VacunaAnimal/sinvacuna'));
+    final jsonData = List.from(jsonDecode(response.body));
+    List<Animalesinvacuna> animales = [];
+    jsonData.forEach((element) {
+      final Animalesinvacuna animals = Animalesinvacuna.fromJson(element);
       animales.add(animals);
     });
     return animales;
   }
-
-  Future<List<Vacuna>> _getAnimalFiltro2() async {
-    List<Vacuna> vacuna = [];
-    jsonData2.forEach((element) {
-      final Vacuna vac = Vacuna.fromJson(element);
-      vacuna.add(vac);
-    });
-
-    return vacuna;
-  }
-}*/
+}
